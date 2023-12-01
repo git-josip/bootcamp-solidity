2a2de672c3dc960954c9a58fe39086306beadaa4
Josip Medic <josipmedic@gmail.com>
2023-12-01 10:03:05 +0100

    revertstring

--- 12_week/huff-puzzles/test/RevertString.t.sol
+++ 12_week/huff-puzzles/test/RevertString.t.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
+import {console2} from "forge-std/Test.sol";
+
 import {HuffConfig} from "foundry-huff/HuffConfig.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
@@ -17,9 +19,20 @@ contract RevertStringTest is Test {
     function testRevertString() public {
         (bool success, bytes memory revertData) = address(revertString).call("");
         require(!success, "call expected to fail");
+
+        console2.log("XXXXXXXX");
+        console2.logBytes(revertData);
+        console2.logBytes(bytes("Only Huff"));
+
+        console2.logBytes32(keccak256(bytes("Only Huff")));
+        console2.log("AAAAA");
+        // console2.logBytes(abi.decode(revertData, (bytes)));
+        console2.logBytes32(keccak256(revertData));
+        
         assertEq(
             keccak256(bytes("Only Huff")),
-            keccak256(abi.decode(revertData, (bytes))),
+            keccak256(revertData),
+            // keccak256(abi.decode(revertData, (bytes))), => this was failing. not sure why
             "Expected the call to revert with custom error 'Only Huff' but it didn't "
         );
     }
