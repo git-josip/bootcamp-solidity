6098f135912de37e1619f69db52058f963ef9f6a
Josip Medic <josipmedic@gmail.com>
2024-02-26 06:31:53 +0100

    backdoor

--- 17_18_weeks/proxies_part_2/dv_defi_12_climber/test/BackdoorTest.t.sol
+++ 17_18_weeks/proxies_part_2/dv_defi_12_climber/test/BackdoorTest.t.sol
@@ -90,52 +90,17 @@ contract BackdoorTest is Test {
         token.transfer(address(walletRegistry), AMOUNT_TOKENS_DISTRIBUTED);
     }
 
-    function test_Exploit() public {
-        /** CODE YOUR EXPLOIT HERE */
-
-        // This challenge require some knowledge how Gnosis Safe contracts works
-        // But I think that it's part of what an auditor should be able to do, read the project code
-        // Understand on which dependencies or other project it does build on top and find what could go wrong
-        // if it integrates with them in the wrong way
-
-        // To create a new proxy wallet via Gnosis Safe you have two options:
-        // 1) using `GnosisSafeProxyFactory.createProxyWithCallback` 
-        // 2) using `GnosisSafeProxyFactory.createProxyWithNonce` 
-        // On top of creating the wallet proxy the `createProxyWithCallback` function have two main differences
+    function test_exploit() public {
+        // To create a new proxy wallet we will use:
+        // `GnosisSafeProxyFactory.createProxyWithCallback` 
         // 1) use the callback as an additional parameter to generate the salt nonce
-        // 2) will call the callback after the wallet has been created
-        // In this case we need to use the first function because otherwise the `WalletRegistry.proxyCreated` callback
-        // would be never called and the DVT token would not be transferred
-
-        // Now that we know how the creation of the proxy works we can see if there are any possible 
-        // problems inside the `WalletRegistry` logic
-        // When the `proxyCreated` callback is triggered by the Gnosis Proxy Factory the registry will check that
-        // 1) it has enough balance to transfer the amount of DVT tokens for each wallet
-        // 2) the `msg.sender` of the callback is indeed the GnosisSafeProxyFactory addresses passed in the `constructor` of the registry. 
-        // By checking this we know that only the whitelisted factory was able to call the callback
-        // 3) the `singleton` used to create the proxy wallet is equal to the `masterCopy` passed in the `constructor` of the registry.
-        // By checking this we know for sure that the wallet is an "authentic" one and does not contains malicius code inside
-        // 4) The initializer used to initialize the proxy is the correct one (the wallet has been correctly initialized)
-        // 5) The wallet threshold of signers needed to execute transaction is equal to 1
-        // 6) The walet number of owners is equal to 1. With those checks the registry know that no one other than the wallet's ownwer
-        // Will be able to administer it (execute tx, add owners, remove owners and so on)
-        // 7) The owner of the wallet is one of the beneficiries listed in the registry. This will prevent the registry to transfer the DVT tokens
-        // to someone that has not been initially whitelisted to receive the tokens
-        // After all these checks it removes the beneficiary from the list of whitelisted one to prevent that someone abuse and get more DVT tokens 
-        // compared to amount it should receive and then transfer the tokens from the registry to the wallet
+        // In this case we need to use the first function because otherwise the `WalletRegistry.proxyCreated` callback would be never called 
 
         // As you can see there are many checks inside this function and it seems pretty safe.
         // Only a wallet with one whitelisted (by the registry) benificiary can receive (only once) 10 DVT token after the callback has been created
         
         // Without the threshold/ownership checks we could have created a wallet with threshold equal to one and with two owners (beneficiary + attacker)
         // and after the transfer we would have executed a transcaction on the wallet to transfer the tokens to the attacker. But this is not a viable option.
-        
-        // We need to understand how a Gnosis Safe wallets are created and if we can do something during the setup process.
-        // These wallets must be very flexible and powerful at the same time to be able to 
-        // - receive ERC20/ERC721/... tokens
-        // - manage the owners/threshold and all the configurations
-        // - be able to execute arbitrary transactions as "call" and "delegatecall"
-        // Flexibility and personalization comes with tradeoffs and footguns if they are not configured and used properly
         
         // Our goal is to find a way to create a wallet, with the correct owner (registry beneficiary), correct configuration
         // but with a "backdoor" to be able to transfer the DVT that the registry transfer to the wallet after the creation
@@ -144,10 +109,8 @@ contract BackdoorTest is Test {
         // there's a `fallbackHandler` parameter... if you look inside the `internalSetFallbackHandler` you will see that
         // if that parameter is setup correctly we are able to replace the wallet `fallback` function with an arbitrary
         // low-level call that will "forward" all the payload to `fallbackHandler` executing it directly from the wallet contract...
-        // Do you see where we are going? Hell yeah! If we setup the `token` as the `fallbackHandler` we will be able 
+        // If we setup the `token` as the `fallbackHandler` we will be able 
         // to execute calls to the token contract directly from the wallet WITHOUT being the owners of the wallet!
-
-
 
         for( uint i = 0; i < beneficiaries.length; i++ ) {
             // setup wallet beneficiary
@@ -180,12 +143,9 @@ contract BackdoorTest is Test {
             assertEq(approveSuccess, true);
         }
 
-        /** SUCCESS CONDITIONS */
-
+        // assertion
         for (uint256 i = 0; i < beneficiaries.length; i++) {
             address wallet = walletRegistry.wallets(beneficiaries[i]);
-            
-            // User must have registered a wallet
             assertTrue(wallet != address(0));
 
             // User is no longer registered as a beneficiary
